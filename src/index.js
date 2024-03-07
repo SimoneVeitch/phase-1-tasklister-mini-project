63bf7dfc316d293cda68ca021169ac5a66eb921f
Simone Veitch <simone.veitch@gmail.com>
2024-03-07 14:39:16 +1100

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText + " ";
+      taskList.appendChild(taskItem);
+      taskInput.value = "";
+      addDeleteButton(taskItem);
+    }
+  });
+
+  function addDeleteButton(taskItem) {
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = " x ";
+    taskItem.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function() {
+      taskItem.remove();
+    });
+  }
 });
